b69445180893f0d6741d8114e64784b4c342f5bd
Julian Müller <julian24816@gmail.com>
2016-09-16 22:16:25 +0200

    fixed bug in main.py

--- main.py
+++ main.py
@@ -15,4 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-print("GUI not yet implemented.")
+
+def main():
+    print("GUI not yet implemented.")
