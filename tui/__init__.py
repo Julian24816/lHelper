b2a1955cff589fa6b4dad3c147bb6e6f2edf3b70
Julian Müller <julian24816@gmail.com>
2016-09-17 08:57:28 +0200

    add docstrings and 'abfragen' and 'einspeichern' commands

--- tui/__init__.py
+++ tui/__init__.py
@@ -1,4 +1,4 @@
-# __init__.py - tui module of lHelper: provide a text based UI for lHelper
+# coding=utf-8
 #
 # Copyright (C) 2016 Julian Mueller
 #
@@ -15,11 +15,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+"""
+tui module of lHelper: provides a text based UI for lHelper
+"""
+
 from tui.menu import Command, MenuOptionsRegistry, mainloop
 
 
 @MenuOptionsRegistry
 class License(Command):
+    """
+    The 'show c' and 'show w' commands.
+    """
     usage = "show c|w"
     description = "show appropriate parts of LICENSE"
 
@@ -33,6 +40,10 @@ class License(Command):
 
     @staticmethod
     def get_warranty():
+        """
+        Reads the warranty part of the LICENSE.
+        :return: the warranty text
+        """
         try:
             f = open("LICENSE")
             lines = f.readlines()
@@ -43,6 +54,10 @@ class License(Command):
 
     @staticmethod
     def get_copyright():
+        """
+        Reads the copyright part of the LICENSE.
+        :return: the copyright text
+        """
         try:
             f = open("LICENSE")
             lines = f.readlines()
@@ -52,11 +67,51 @@ class License(Command):
             return "file LICENSE not found."
 
 
+@MenuOptionsRegistry
+class Abfragen(Command):
+    """
+    The 'abfragen' command.
+    """
+    usage = "abfragen [max_shelf]"
+    description = "starts the 'abfragen' cycle"
+
+    def __init__(self, shelf=5):
+        try:
+            shelf = int(shelf)
+        except ValueError:
+            raise TypeError
+        if shelf not in range(8):
+            print("max_shelf must lay between 0 and 7.")
+            return
+
+        print("abfragen", shelf)
+
+    @classmethod
+    def get_help(cls):
+        """
+        Returns a help string for the 'abfragen' command.
+        :return: the help string
+        """
+        return "{}\n{}\n\n{}".format(cls.usage_notice(), cls.description, "max_shelf : the max shelf to be included")
+
+
+@MenuOptionsRegistry
+class Einspeichern(Command):
+    """
+    The 'einspeichern' command.
+    """
+    usage = "einspeichern"
+    description = "starts the 'einspeichern' cycle"
+
+
 def main():
+    """
+    The TextUIs main method.
+    :return:
+    """
     print("""lHelper Copyright (C) 2016 Julian Mueller
 This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
 This is free software, and you are welcome to redistribute it
 under certain conditions; type 'show c' for details.""")
 
     mainloop(prompt="$ ")
-
