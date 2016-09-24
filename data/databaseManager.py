8b75d3f558f4d1206ffd49d01bb7320417d0939a
Julian Müller <julian24816@gmail.com>
2016-09-24 22:09:37 +0200

    implemented all previously available DataManager methods

--- data/databaseManager.py
+++ data/databaseManager.py
@@ -23,6 +23,9 @@ from data.databaseOpenHelper import *
 from data.databaseConstants import *
 from typing import List, Optional, Tuple
 
+Card = List[Tuple[str, str, str, str]]
+Group = Tuple[str, Optional[str], List[Card]]
+
 
 class DatabaseManager(DatabaseOpenHelper):
     """
@@ -47,7 +50,7 @@ class DatabaseManager(DatabaseOpenHelper):
 
     def load_ids(self):
         """
-        loads the current max ids from the database.
+        Loads the current max ids from the database.
         """
         db = self.get_connection()
         cur = db.cursor()
@@ -67,8 +70,8 @@ class DatabaseManager(DatabaseOpenHelper):
 
     def create_tables(self):
         """
-        Overrides DatabaseOpenHelper.create_tables()
         Creates the database tables if not present.
+        Overrides DatabaseOpenHelper.create_tables().
         """
         db = self.get_connection()
         cur = db.cursor()
@@ -164,7 +167,7 @@ class DatabaseManager(DatabaseOpenHelper):
                                       + TRANSLATION_PHRASE_1 + "=? AND " + TRANSLATION_PHRASE_2 + "=?;",
                                       (phrase_id_1, phrase_id_2)).fetchone()[0]
 
-    def add_card(self, translations: List[Tuple[str, str, str, str]], cursor: Cursor = None) -> int:
+    def add_card(self, translations: Card, cursor: Cursor = None) -> int:
         """
         Adds a card with translations to the database and returns the cards id.
         :param translations: a list of str-4-tuples describing the translations on the card
@@ -191,10 +194,11 @@ class DatabaseManager(DatabaseOpenHelper):
                                + ") VALUES (?,?);", (self.card_id, translation_id))
             return self.card_id
 
-    def add_group(self, group: Tuple[str, Optional[str]], cursor: Cursor = None) -> int:
+    def add_group(self, group_name: str, parent_name: str = None, cursor: Cursor = None) -> int:
         """
         Tries to add a group to the database and returns the groups id independent of success.
-        :param group: the group to be added
+        :param group_name: the groups name
+        :param parent_name: the groups parent
         :param cursor: the cursor to be used to access the database
         :return: the groups id
         """
@@ -203,7 +207,7 @@ class DatabaseManager(DatabaseOpenHelper):
         if cursor is None:
             db = self.get_connection()
             cur = db.cursor()
-            group_id = self.add_group(group, cur)
+            group_id = self.add_group(group_name, parent_name, cur)
             db.commit()
             db.close()
             return group_id
@@ -211,13 +215,13 @@ class DatabaseManager(DatabaseOpenHelper):
         # a cursor was passed on
         else:
             # retrieve the parent_id
-            parent = self.add_group((group[1], None), cursor) if group[1] else None
+            parent_name = self.add_group(parent_name, cursor=cursor) if parent_name else None
 
             # try to add the group to the database
             try:
                 cursor.execute("INSERT INTO " + TABLE_GROUP + "("
                                + ",".join((GROUP_ID, GROUP_NAME, GROUP_PARENT))
-                               + ") VALUES (?,?,?);", (self.group_id + 1, group[0], parent))
+                               + ") VALUES (?,?,?);", (self.group_id + 1, group_name[0], parent_name))
 
                 # insert succeeded
                 self.group_id += 1
@@ -228,131 +232,148 @@ class DatabaseManager(DatabaseOpenHelper):
 
                 # load the existing groups's id
                 return cursor.execute("SELECT " + GROUP_ID + " FROM " + TABLE_GROUP + " WHERE "
-                                      + GROUP_NAME + "=?;", (group[0],)).fetchone()[0]
+                                      + GROUP_NAME + "=?;", (group_name[0],)).fetchone()[0]
 
+    def add_card_to_group(self, card_id: int, group_name: str, cursor: Cursor = None):
+        """
+        Adds a card to a group in the database. Adds the group if needed.
+        :param card_id: the id of the card to be added
+        :param group_name: the name of the group the card will be added to
+        :param cursor: the cursor to be used to access the database
+        """
 
-'''
-    def load_card(self, card_id: int, cursor: sqlite3.Cursor = None) -> Card:
-        """
-        Loads a card from the database accessed by cursor.
-        :param card_id: the card's id
-        :param cursor: the cursor to be used
-        :return: the loaded Card
-        """
+        # if no cursor was passed on, open the database and call the method recursively with a new cursor object
         if cursor is None:
             db = self.get_connection()
-            curs = db.cursor()
-            card = self.load_card(card_id, curs)
+            cur = db.cursor()
+            self.add_card_to_group(card_id, group_name, cur)
+            db.close()
+
+        # a cursor was passed on
+        else:
+            # retrieve the group_id
+            group_id = self.add_group(group_name)
+
+            try:
+                cursor.execute("INSERT INTO " + TABLE_CARD_GROUP + "(" + ",".join((GROUP_ID, CARD_ID)) + ")"
+                               + " VALUES (?,?);", (group_id, card_id))
+
+            # the card is already in group group_name
+            except IntegrityError:
+                pass
+
+    def get_card(self, card_id: int, cursor: Cursor = None) -> Card:
+        """
+        Loads a card from the database.
+        :param card_id: the cards id
+        :param cursor: the cursor to be used to access the database
+        :return: a list of str-4-Tuples representing the cards translations
+        """
+
+        # if no cursor was passed on, open the database and call the method recursively with a new cursor object
+        if cursor is None:
+            db = self.get_connection()
+            cur = db.cursor()
+            card = self.get_card(card_id, cur)
             db.close()
             return card
+
+        # a cursor was passed on
         else:
-            cursor.execute("SELECT " + ", ".join(["l." + WORD_ROOT_FORMS, "l." + WORD_ANNOTATIONS,
-                                                  "lu." + USAGE_CONTEXT, "g." + WORD_ROOT_FORMS,
-                                                  "g." + WORD_ANNOTATIONS, "gu." + USAGE_CONTEXT]) +
-                           " FROM " + TABLE_CARD + " AS c" +
-                           " JOIN " + TABLE_TRANSLATION + " AS t ON c." + TRANSLATION_ID + "=t." + TRANSLATION_ID +
-                           " JOIN " + TABLE_USAGE + " AS lu ON t." + TRANSLATION_LATIN_USAGE_ID + "=lu." + USAGE_ID +
-                           " JOIN " + TABLE_WORD + " AS l ON lu." + WORD_ID + "=l." + WORD_ID +
-                           " JOIN " + TABLE_USAGE + " AS gu ON t." + TRANSLATION_GERMAN_USAGE_ID + "=gu." + USAGE_ID +
-                           " JOIN " + TABLE_WORD + " AS g ON gu." + WORD_ID + "=g." + WORD_ID +
-                           " WHERE c." + CARD_ID + "=" + str(card_id)
-                           )
+            cursor.execute("SELECT " + ",".join(["l1." + PHRASE_DESCRIPTION, "l1." + PHRASE_LANGUAGE,
+                                                 "l2." + PHRASE_DESCRIPTION, "l2." + PHRASE_LANGUAGE])
+                           + " FROM " + TABLE_CARD + " AS c"
+                           + " JOIN " + TABLE_TRANSLATION + " AS t ON c." + TRANSLATION_ID + "=t." + TRANSLATION_ID
+                           + " JOIN " + TABLE_PHRASE + " AS l1 ON t." + TRANSLATION_PHRASE_1 + "=l1." + PHRASE_ID
+                           + " JOIN " + TABLE_PHRASE + " AS l2 ON t." + TRANSLATION_PHRASE_2 + "=l2." + PHRASE_ID
+                           + " WHERE c." + CARD_ID + "=?;", (card_id,))
 
-            translations = []
-            for l_root, l_annotation, l_context, g_root, g_annotation, g_context in cursor.fetchall():
-                translations.append(Translation(Usage(Word(l_root, l_annotation, "latin"), l_context),
-                                                Usage(Word(g_root, g_annotation, "german"), g_context)))
+            return cursor.fetchall()
 
-            return Card(translations, card_id)
-
-    def load_group(self, group_id: int, cursor: sqlite3.Cursor = None) -> CardGroup:
+    def load_group(self, group_id: int, cursor: Cursor = None) -> Group:
         """
-        Loads a CardGroup from the database
+        Loads a group from the database.
         :param group_id: the groups id
-        :return: the CardGroup
+        :param cursor: the cursor to be used to access the database.
+        :return: a tuple with the group_name, the groups parent_name or None, and  a list of cards
         """
+
+        # if no cursor was passed on, open the database and call the method recursively with a new cursor object
         if cursor is None:
             db = self.get_connection()
             cur = db.cursor()
             group = self.load_group(group_id, cur)
             db.close()
             return group
-        else:
-            cursor.execute("SELECT " + GROUP_NAME + " FROM " + TABLE_GROUP
-                           + " WHERE " + GROUP_ID + "=?", (group_id,))
-            group_name = cursor.fetchone()
-            if group_name is None:
-                raise ValueError("group_id does not exist.")
-            group = CardGroup(group_name[0], [])
-            cursor.execute("SELECT " + CARD_ID +
-                           " FROM " + TABLE_CARD_GROUP +
-                           " WHERE " + GROUP_ID + " IN (" + ",".join(
-                map(str, self.get_subgroup_ids(group_id, cursor))) + ")")
-            for card_id, in cursor.fetchall():
-                group.add_card(self.load_card(card_id, cursor))
-            return group
 
-    # noinspection PyMethodMayBeStatic
-    def get_subgroup_ids(self, group_id: int, cursor: sqlite3.Cursor) -> List[int]:
+        # a cursor was passed on
+        else:
+            # load group name and parent name
+            cursor.execute("SELECT " + ",".join((GROUP_NAME, GROUP_PARENT)) + " FROM " + TABLE_GROUP + " WHERE "
+                           + GROUP_ID + "=?;", (group_id,))
+            name, parent = cursor.fetchone()
+
+            # load cards
+            cursor.execute("SELECT " + CARD_ID + " FROM " + TABLE_CARD_GROUP + " WHERE " + GROUP_ID
+                           + " IN (" + ",".join(map(str, self.get_subgroup_ids(group_id, cursor))) + ");")
+
+            cards = [self.get_card(row[0], cursor) for row in cursor.fetchall()]
+
+            return name, parent, cards
+
+    def get_subgroup_ids(self, parent: int, cursor: Cursor = None) -> List[int]:
         """
-        Loads all subgroups of group_id from the database and subgroups of these ...
-        :param group_id: the starting group_id
-        :param cursor: the database cursor to be used
+        Loads the ids of all subgroups of parent and their subgroups recursively.
+        :param parent: the parents id
+        :param cursor: the cursor to be used to access the database
         :return: a list of group_ids
         """
-        ids = [group_id]
-        for group in ids:
-            cursor.execute("SELECT " + GROUP_ID + " FROM " + TABLE_GROUP
-                           + " WHERE " + GROUP_PARENT + "=?", (group,))
-            for el, in cursor:
-                if el not in ids:
-                    ids.append(el)
-        return ids
 
-    def add_card_to_group(self, card: Card, group: CardGroup):
+        # if no cursor was passed on, open the database and call the method recursively with a new cursor object
+        if cursor is None:
+            db = self.get_connection()
+            cur = db.cursor()
+            group_ids = self.get_subgroup_ids(parent, cur)
+            db.close()
+            return group_ids
+
+        # a cursor was passed on
+        else:
+            group_ids = [parent]
+            for group_id in group_ids:
+                cursor.execute("SELECT " + GROUP_ID + " FROM " + TABLE_GROUP + " WHERE " + GROUP_PARENT + "=?;",
+                               (group_id,))
+                group_ids.extend(map(lambda row: row[0], cursor.fetchall()))
+            return group_ids
+
+    def get_group_id_for_name(self, group_name: int, cursor: Cursor = None) -> str:
         """
-        Adds a card to a group in the database. Adds the group if needed.
-        :param card: the card to be added to the group
-        :param group: the group the card should be added to
+        Loads a groups id from the database.
+        :param group_name: the groups name
+        :param cursor: the cursor to be used to access the database
+        :return: the groups id
         """
-        db = self.get_connection()
-        cur = db.cursor()
-        group_id = self.add_group(group, cur)
 
-        cur.execute("INSERT INTO " + TABLE_CARD_GROUP + "("
-                    + ",".join((GROUP_ID, CARD_ID))
-                    + ") VALUES (?,?)", (group_id, card.Id))
+        # if no cursor was passed on, open the database and call the method recursively with a new cursor object
+        if cursor is None:
+            db = self.get_connection()
+            cur = db.cursor()
+            group_id = self.get_subgroup_ids(group_name, cur)
+            db.close()
+            return group_id
 
-        db.commit()
-        db.close()
+        # a cursor was passed on
+        else:
+            return cursor.execute("SELECT "+GROUP_ID+" FROM "+TABLE_GROUP+" WHERE "+GROUP_NAME+"=?",
+                                  (group_name,)).fetchone()[0]
 
     def get_all_group_names(self) -> List[str]:
         """
-        Loads all card group names
+        Loads all card group names.
         :return: a list of card group names
         """
         db = self.get_connection()
-        cur = db.cursor()
-        cur.execute("SELECT " + GROUP_NAME + " FROM " + TABLE_GROUP)
-        names = list(map(lambda e: e[0], cur.fetchall()))
+        names = list(map(lambda row: row[0],
+                         db.cursor().execute("SELECT " + GROUP_NAME + " FROM " + TABLE_GROUP).fetchall()))
         db.close()
         return names
-
-    def get_group_for_name(self, group_name) -> CardGroup:
-        """
-        Loads the group with name group_name
-        :param group_name: the group's name
-        :return: a CardGroup
-        """
-        db = self.get_connection()
-        cur = db.cursor()
-        cur.execute("SELECT " + GROUP_ID + " FROM " + TABLE_GROUP + " WHERE " + GROUP_NAME + "=?", (group_name,))
-        group_id = cur.fetchone()
-        if group_id is not None:
-            group_id = group_id[0]
-        else:
-            raise ValueError("group {} does not exist".format(group_name))
-        group = self.load_group(group_id, cur)
-        db.close()
-        return group
-'''
