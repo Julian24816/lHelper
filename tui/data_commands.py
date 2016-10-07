5e37ceebad80276409336b8a98a9a07c7a0a208d
Julian Müller <julian24816@gmail.com>
2016-10-07 17:37:27 +0200

    remove command walk

--- tui/data_commands.py
+++ tui/data_commands.py
@@ -27,68 +27,6 @@ from tui.edit import edit_card
 from data import database_manager
 
 
-# todo refactor walk
-'''
-@MenuOptionsRegistry
-class WalkCards(Command):
-    """
-    The 'walk' command.
-    """
-    usage = "walk group action"
-    description = "walk all cards in group and perform the action on them"
-
-    def __init__(self, group, action):
-        group_names = card_manager.get_all_group_names()
-
-        # search for exactly one '>'
-        if match("^[^>]+>[^>]+$",group):
-            group, gt = group.split(">")
-        else:
-            gt = None
-
-        # load group
-        if group == "all":
-            print("loading cards ...")
-            all_cards = card_manager.get_all_cards()
-            if gt:
-                cards = []
-                for card in all_cards:
-                    if card.get_translations()[0].latinUsage.word.root_forms > gt:
-                        cards.append(card)
-            else:
-                cards = all_cards
-
-        elif group in group_names:
-            print("loading cards ...")
-            cards = card_manager.get_card_group_for_name(group).cards
-
-        else:
-            print("group unknown")
-            return
-
-        # determine action
-        if action == "repeat":
-            repeat(cards)
-        elif action == "assign_group":
-            assign_group(cards)
-        else:
-            print("action unknown")
-            return
-
-    @classmethod
-    def get_help(cls):
-        """
-        Returns a help string for the 'walk' command.
-        :return: the help string
-        """
-        to_return = cls.usage_notice() + "\n\n" + cls.description + "\n\n"
-        to_return += "  group  : the group to be affected: a group_name or 'all'\n"
-        to_return += "           a '>word' str may be appended to all for the command to only affect cards > word\n"
-        to_return += "  action : the action to be performed: assign_group or repeat"
-        return to_return
-'''
-
-
 @MenuOptionsRegistry
 class Add(Command):
     """
